8024b35e84a76c3939bb80980681a25a701c399d
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-02 18:13:09 +0000

    added a timer and death to golumn

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -10,27 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
 
+    @IBOutlet weak var penalty1: UIImageView!
+    @IBOutlet weak var penalty2: UIImageView!
+    @IBOutlet weak var penalty3: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var imgArray = [UIImage]()
-        for var x = 1; x <= 4; x++ {
-            let img = UIImage(named: "idle\(x).png")
-            imgArray.append(img!)
-        }
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
         
-        monsterImg.animationImages = imgArray
-        monsterImg.animationDuration = 0.8
-        monsterImg.animationRepeatCount = 0
-        monsterImg.startAnimating()
+        penalty1.alpha = DIM_ALPHA
+        penalty2.alpha = DIM_ALPHA
+        penalty3.alpha = DIM_ALPHA
+        
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+        
+        startTimer()
         
     }
-
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("Item dropped on Character")
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    func changeGameState() {
+        
+        penalties++
+        
+        if penalties == 1 {
+            penalty1.alpha = OPAQUE
+            penalty2.alpha = DIM_ALPHA
+            
+        } else if penalties == 2 {
+            penalty2.alpha = OPAQUE
+            penalty3.alpha = DIM_ALPHA
+            
+        } else if penalties >= 3 {
+            penalty3.alpha = OPAQUE
+        
+        } else {
+            penalty1.alpha = DIM_ALPHA
+            penalty2.alpha = DIM_ALPHA
+            penalty3.alpha = DIM_ALPHA
+        }
+        
+        if penalties >= MAX_PENALTIES {
+            gameOver()
+        }
+        
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+    }
 
 }
 
